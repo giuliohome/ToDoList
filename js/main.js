bc52968944dec6193e32610b1e4abadeb64e972e
giuliohome <giuliohome@gmail.com>
2020-11-28 11:31:33 +0100

    at 1:26:03

--- js/main.js
+++ js/main.js
@@ -12,15 +12,22 @@ document.addEventListener("readystatechange", (event) => {
 
 const initApp = () => {
     //Add listeners
+    const itemEntryForm = document.getElementById("itemEntryForm");
+    itemEntryForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        processSubmission();
+    })
 
+    // Procedural
+    // load the list object
     refreshThePage();
 }
 
 const refreshThePage = () => {
     clearListDispay();
     renderList();
-    // clearItemEntryField();
-    // setFocusOnItemEntry();
+    clearItemEntryField();
+    setFocusOnItemEntry();
 }
 
 const clearListDispay = () => {
@@ -44,5 +51,66 @@ const renderList = () => {
 }
 
 const buildListItem = (item) => {
-
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+    addClickListenerToCheckbox(check)
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getItem();
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById("listItems");
+    container.appendChild(div);
 }
+
+const addClickListenerToCheckbox = (checkbox) => {
+    checkbox.addEventListener("click", (event) => {
+        toDoList.removeItemFromList(checkbox.id);
+        // TODO: remove from persistent data
+        setTimeout(() => {
+            refreshThePage();
+        }, 1000); 
+    });
+}
+
+const clearItemEntryField = () => {
+    document.getElementById("newItem").value = "";
+};
+
+const setFocusOnItemEntry = () => {
+    document.getElementById("newItem").focus();
+};
+
+const processSubmission = () => {
+    const newEntryText = getNewEntry();
+    if (!newEntryText.length) return;
+    const nextItemId = calcNextItemId();
+    const toDoItem = createNewItem(nextItemId, newEntryText);
+    toDoList.addItemToList(toDoItem);
+    // TODO update persistent data
+    refreshThePage();
+};
+
+const getNewEntry = () => {
+    return document.getElementById("newItem").value.trim();
+};
+
+const calcNextItemId = () => {
+    let nextItemId = 1;
+    const list = toDoList.getList();
+    if (list.length > 0) {
+        nextItemId = list[list.length - 1].getId() + 1;
+    }
+    return nextItemId;
+};
+
+const createNewItem =  (itemId, itemText) => {
+    const toDo = new ToDoItem();
+    toDo.setId(itemId);
+    toDo.setItem(itemText);
+    return toDo;
+};
