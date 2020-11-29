0b2209882e9ffe59ec314ca228197adcea1aab25
giuliohome <giuliohome@gmail.com>
2020-11-29 18:39:31 +0100

    tutorial completed

--- js/main.js
+++ js/main.js
@@ -96,10 +96,16 @@ const addClickListenerToCheckbox = (checkbox) => {
     checkbox.addEventListener("click", (event) => {
         toDoList.removeItemFromList(checkbox.id);
         updatePersistentData(toDoList.getList());
+        const removedText = getLabelText(checkbox.id);
+        updateScreenReaderConfirmation(removedText, "removed from list");
         setTimeout(() => {
             refreshThePage();
         }, 1000); 
     });
+}
+
+const getLabelText = (checkboxId) => {
+    return document.getElementById(checkboxId).nextElementSibling.textContent; 
 }
 
 const updatePersistentData = (listArray) => {
@@ -121,6 +127,7 @@ const processSubmission = () => {
     const toDoItem = createNewItem(nextItemId, newEntryText);
     toDoList.addItemToList(toDoItem);
     updatePersistentData(toDoList.getList());
+    updateScreenReaderConfirmation(newEntryText, "added");
     refreshThePage();
 };
 
@@ -142,4 +149,10 @@ const createNewItem =  (itemId, itemText) => {
     toDo.setId(itemId);
     toDo.setItem(itemText);
     return toDo;
+};
+
+const updateScreenReaderConfirmation = (newEntryText, actionVerb) => {
+    document.getElementById(
+        "confirmation"
+    ).textContent = `${newEntryText} ${actionVerb}.`;
 };
